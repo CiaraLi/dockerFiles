f345546a76a32ec10bc7637ea2e959ff8341a002
liwei24 <liwei24@100tal.com>
2021-11-22 11:49:22 +0800

    maven
    maven安装

--- maven/src/maven_install.sh
+++ maven/src/maven_install.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-homeDir="/var/data" 
-jdk="java8"
-tomcat="tomcat"
-webapp="webapp" 
+homeDir="/usr/local/"
+tomcat="tomcat" 
  
 #------设置账号和服务  提前在src 下面 准备好tomcat-users.xml  server.xml 文件 
 if [ ! -f  "$homeDir/src/tomcat-users.xml" ]  
@@ -16,5 +14,11 @@ then
   	echo ""
 else
 	rm  -f   /usr/local/$tomcat/conf/server.xml && cp $homeDir/src/server.xml       /usr/local/$tomcat/conf/
+fi
+if [ ! -f  "$homeDir/src/manager.xml" ]  
+then
+  	echo ""
+else
+	mkdir -p   /usr/local/$tomcat/conf/Catalina/localhost/ && cp    $homeDir/src/manager.xml      /usr/local/$tomcat/conf/Catalina/localhost/
 fi
 rm -rf  $homeDir/src/* 
